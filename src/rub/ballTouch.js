0e8418dd0751e8b486b0d91a79209d27d183058f
rubebeben <55009611+rubebeben@users.noreply.github.com>
2020-03-29 22:23:16 -0300

    Update ballTouch.js

--- src/rub/ballTouch.js
+++ src/rub/ballTouch.js
@@ -8,7 +8,7 @@ room.pluginSpec = {
 
 var playersThatTouchedTheBall = [];
 
-function pointDistance ( p1, p2 ) {
+function pointDistance ( p1, p2 ){
 	let d1 = p1.x - p2.x;
 	let d2 = p1.y - p2.y;
 	return Math.sqrt( d1 * d1 + d2 * d2 );

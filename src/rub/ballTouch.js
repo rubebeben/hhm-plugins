cb3c29556bb2d1ed8ce9b3077a1f0802732e38da
rubebeben <55009611+rubebeben@users.noreply.github.com>
2020-03-29 22:22:50 -0300

    Update ballTouch.js

--- src/rub/ballTouch.js
+++ src/rub/ballTouch.js
@@ -8,13 +8,13 @@ room.pluginSpec = {
 
 var playersThatTouchedTheBall = [];
 
-function pointDistance(p1, p2) {
+function pointDistance ( p1, p2 ) {
 	let d1 = p1.x - p2.x;
 	let d2 = p1.y - p2.y;
-	return Math.sqrt(d1 * d1 + d2 * d2);
+	return Math.sqrt( d1 * d1 + d2 * d2 );
 }
 
-function onGameTickHandler() {
+function onGameTickHandler () {
 	let players = room.getPlayerList();
 	let ballPosition = room.getBallPosition();
 	let ballRadius = room.getDiscProperties(0).radius;
@@ -34,12 +34,12 @@ function onGameTickHandler() {
 		// This check is here so that the event is only notified the first game tick in which the player is touching the ball.
 		if ( distanceToBall < triggerDistance ) {
 			if ( !hadTouchedTheBall ) {
-				room.sendAnnouncement( "[1] " + player.name + " touch the ball." );
+				// room.sendAnnouncement( "[1] " + player.name + " touch the ball." ); // DEBUG
 				playersThatTouchedTheBall.unshift(player.id);
 			}
 
 			else {
-				room.sendAnnouncement( "[2] " + player.name + " touch the ball." );
+				// room.sendAnnouncement( "[2] " + player.name + " touch the ball." ); // DEBUG
 				playersThatTouchedTheBall.splice(playersThatTouchedTheBall.indexOf(player.id), 1);
 				playersThatTouchedTheBall.unshift(player.id);
 			}
@@ -50,13 +50,13 @@ function onGameTickHandler() {
 function onPlayerBallKickHandler ( player ) {
   let index = playersThatTouchedTheBall.indexOf( player.id );
 	if ( index != -1 ) {
-		room.sendAnnouncement( "[1] " + player.name + " kick the ball." );
+		// room.sendAnnouncement( "[1] " + player.name + " kick the ball." ); // DEBUG
 		playersThatTouchedTheBall.splice( index, 1 );
 		playersThatTouchedTheBall.unshift( player.id );
 	}
 	else {
-	 	room.sendAnnouncement( "[2] " + player.name + " kick the ball." );
-  		playersThatTouchedTheBall.unshift( player.id );
+		// room.sendAnnouncement( "[2] " + player.name + " kick the ball." ); // DEBUG
+		playersThatTouchedTheBall.unshift( player.id );
   }
 }
 

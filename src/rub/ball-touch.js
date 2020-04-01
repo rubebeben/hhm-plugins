16e5eb561ec7027b8fdaf2b2a64db12085b35e08
rubebeben <55009611+rubebeben@users.noreply.github.com>
2020-04-01 14:29:35 -0300

    Update ball-touch.js

--- src/rub/ball-touch.js
+++ src/rub/ball-touch.js
@@ -18,26 +18,24 @@ function onGameTickHandler () {
 	let players = room.getPlayerList();
 	let ballPosition = room.getBallPosition();
 	let ballRadius = room.getDiscProperties(0).radius;
-	
 	for ( let i = 0; i < players.length; i++ ) { // Iterate over all the players
-		let player = players[i];
+
+	  let player = players[i];
+
 		if ( player.position == null ) continue; // Skip players that don't have a position
 
 		let playerRadius = room.getPlayerDiscProperties(player.id).radius;
 		let triggerDistance = ballRadius + playerRadius + 0.01;
-
 		let distanceToBall = pointDistance(player.position, ballPosition);
 		let hadTouchedTheBall = playersThatTouchedTheBall.some((element) => element == player.id);
-
+		
 		if ( playersThatTouchedTheBall.length > 3 ) playersThatTouchedTheBall.pop();
-
 		// This check is here so that the event is only notified the first game tick in which the player is touching the ball.
 		if ( distanceToBall < triggerDistance ) {
 			if ( !hadTouchedTheBall ) {
 				// room.sendAnnouncement( "[1] " + player.name + " touch the ball." ); // DEBUG
 				playersThatTouchedTheBall.unshift(player.id);
 			}
-
 			else {
 				// room.sendAnnouncement( "[2] " + player.name + " touch the ball." ); // DEBUG
 				playersThatTouchedTheBall.splice(playersThatTouchedTheBall.indexOf(player.id), 1);
@@ -48,7 +46,7 @@ function onGameTickHandler () {
 }
 
 function onPlayerBallKickHandler ( player ) {
-  let index = playersThatTouchedTheBall.indexOf( player.id );
+	let index = playersThatTouchedTheBall.indexOf( player.id );
 	if ( index != -1 ) {
 		// room.sendAnnouncement( "[1] " + player.name + " kick the ball." ); // DEBUG
 		playersThatTouchedTheBall.splice( index, 1 );
@@ -57,7 +55,7 @@ function onPlayerBallKickHandler ( player ) {
 	else {
 		// room.sendAnnouncement( "[2] " + player.name + " kick the ball." ); // DEBUG
 		playersThatTouchedTheBall.unshift( player.id );
-  }
+	}
 }
 
 function onPositionsResetHandler () {

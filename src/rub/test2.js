bee448ba0c2c72585bd7dede0fec028ae42ae313
rubebeben <55009611+rubebeben@users.noreply.github.com>
2020-04-01 14:44:24 -0300

    Update test2.js

--- src/rub/test2.js
+++ src/rub/test2.js
@@ -4,12 +4,27 @@ room.pluginSpec = {
   name: `rub/test2`,
   author: `ruben`,
   version: `1.0.0`,
+  config: {},
+  dependencies: [
+    `rub/test1`,
+  ],
+  order: {
+    'after': [`rub/test1`],
+	},
+  incompatible_with: [],
 };
 
-function onPlayerJoinHandler ( player ) {
-	room.sendAnnouncement( "[2] " + player.name + msg );
+
+let players = room.getPlugin( `rub/ball-touch` ).getPlayersObject();
+
+function onPlayerChatHandler ( player, message ) {
+	if ( message == "a" ) console.dir( players );
+	else{
+		players[player.id].message = [];
+		players[player.id].message.push( message );
+	}
 }
 
 room.onRoomLink = function onRoomLink() {
-  room.onPlayerJoin = onPlayerJoinHandler;
+  room.onPlayerChat = onPlayerChatHandler;
 }

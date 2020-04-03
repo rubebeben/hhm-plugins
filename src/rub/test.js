9bd4177b9182e7281dac1ef71bbf5463fbe4d86c
rubebeben <55009611+rubebeben@users.noreply.github.com>
2020-04-03 15:06:26 -0300

    Create test.js

---
+++ src/rub/test.js
@@ -0,0 +1,32 @@
+var room = HBInit();
+
+room.pluginSpec = {
+  name: `rub/test`,
+  author: `ruben`,
+  version: `1.0.0`,
+  config: {},
+  dependencies: [
+    `rub/match-info`,
+  ],
+  order: {},
+  incompatible_with: [],
+};
+
+function onTeamScoresHandler ( team, goal ) {
+  if ( goal.ownGoal ) {
+    room.sendAnnouncement( `Scored by ${room.getPlayer( goal.scorer ).name} (own goal) ` );
+  }
+  else {
+    room.sendAnnouncement( `Scored by ${room.getPlayer( goal.scorer ).name} ${ goal.assister ? `assited by ${room.getPlayer( goal.assister ).name}` : false } ` );
+  }
+}
+
+function onMatchEndHandler ( scores, gks ) {
+  room.sendAnnouncement( `goalkeeper red : ${room.getPlayer( gks.red ).name}` );
+  room.sendAnnouncement( `goalkeeper blue : ${room.getPlayer( gks.blue ).name}` );
+}
+
+room.onRoomLink = () => {
+  room.onMatchEnd = onMatchEndHandler;
+  room.onTeamScores = onTeamScoresHandler;
+}

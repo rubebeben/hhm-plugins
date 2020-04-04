e21dbd24bf759a70bbe71e78b467067e3858ea9b
rubebeben <55009611+rubebeben@users.noreply.github.com>
2020-04-04 15:25:50 -0300

    Create team-state.js

---
+++ src/rub/team-state.js
@@ -0,0 +1,43 @@
+
+var room = HBInit();
+
+room.pluginSpec = {
+  name: `rub/team-state`,
+  author: `ruben`,
+  version: `1.0.0`,
+  config: {},
+  dependencies: [],
+  order: {},
+  incompatible_with: [],
+};
+
+let teams = [ [], [], [], ];
+
+function removePlayerTeam ( player ) {
+  for ( let i = 0 ; i < 3 ; i++ ) {
+    let index = teams[i].indexOf( player.id );
+    if ( index != -1 ) {
+      teams[i].splice( index, 1 );
+      break;
+    }
+  }
+}
+
+function onPlayerJoinHandler ( player ) {
+  teams[player.team].push( player.id );
+}
+
+function onPlayerLeaveHandler ( player ) {
+  removePlayerTeam( player );
+}
+
+function onPlayerTeamChangeHandler ( player ) {
+  removePlayerTeam( player );
+  teams[player.team].push( player.id );
+}
+
+room.onRoomLink = function onRoomLink () {
+  room.onPlayerTeamChange = onPlayerTeamChangeHandler;
+  room.onPlayerLeave = onPlayerLeaveHandler;
+  room.onPlayerJoin = onPlayerJoinHandler;
+}

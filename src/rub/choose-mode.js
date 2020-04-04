df94045cea3e1bfac0f8ec67ac3c6634f3b284e5
rubebeben <55009611+rubebeben@users.noreply.github.com>
2020-04-04 14:34:02 -0300

    Update choose-mode.js

--- src/rub/choose-mode.js
+++ src/rub/choose-mode.js
@@ -5,8 +5,18 @@ room.pluginSpec = {
   name: `rub/choose-mode`,
   author: `ruben`,
   version: `1.0.0`,
-  config: {},
-  dependencies: [],
+  config: {
+    enabled : false,
+    allowedRoles : ['admin','host'],
+  },
+  dependencies: [
+    `sav/commands`,
+    `sav/roles`,
+  ],
   order: {},
   incompatible_with: [],
 };
+
+const config = room.getConfig();
+
+let players = [];

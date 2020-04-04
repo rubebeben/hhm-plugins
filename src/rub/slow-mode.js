336b521b09cc64fd27e1ca39f822b461b9bbbea6
rubebeben <55009611+rubebeben@users.noreply.github.com>
2020-04-04 14:29:15 -0300

    Update slow-mode.js

--- src/rub/slow-mode.js
+++ src/rub/slow-mode.js
@@ -1,1 +1,12 @@
 
+var room = HBInit();
+
+room.pluginSpec = {
+  name: `rub/votes`,
+  author: `ruben`,
+  version: `1.0.0`,
+  config: {},
+  dependencies: [],
+  order: {},
+  incompatible_with: [],
+};

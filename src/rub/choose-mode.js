21ed5257c4653f0b6f08ccd45295ac445d12b018
rubebeben <55009611+rubebeben@users.noreply.github.com>
2020-04-04 15:51:37 -0300

    Update choose-mode.js

--- src/rub/choose-mode.js
+++ src/rub/choose-mode.js
@@ -12,11 +12,46 @@ room.pluginSpec = {
   dependencies: [
     `sav/commands`,
     `sav/roles`,
+    `rub/team-state`,
   ],
   order: {},
   incompatible_with: [],
 };
 
 const config = room.getConfig();
+const teams = room.getPlugin(`rub/team-state`).getTeamState();
+const teamID = {
+  SPECTATORS : 0,
+  RED : 1,
+  BLUE : 2,
+}
 
 let players = [];
+
+room.triggerEvent("chooseMode", {...scores}, {...goalkeepers});
+
+function chooseModeHandler () {
+  
+}
+
+let teamThatMustChoose = teams[1].length != teams[2].length ? ( teams[1].length < teams[2].length ? 1 : 2 ) : false;
+
+function onPlayerChatHandler ( player, message ) {
+  
+  if ( player.team != 0 && player.team == teamThatMustChoose )
+  if ( message > 0 && message <= teams[0].length )
+}
+
+// !room.getPlayerList().some( (e) => e.admin )
+
+function onPlayerLeaveHandler ( player ) {
+  if ( player.team != 0 ) room.triggerEvent("chooseMode", [...args]);
+}
+
+room.onRoomLink = () => {
+  room.onPlayerLeave = onPlayerLeaveHandler;
+  room.onPlayerChat = onPlayerChatHandler;
+  room.onPlayerTeamChange = onPlayerTeamChangeHandler;
+  room.chooseMode = chooseModeHandler;
+}
+

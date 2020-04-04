eef0646546e7f05b8b79f7f922358c23e43f731f
rubebeben <55009611+rubebeben@users.noreply.github.com>
2020-04-04 14:29:42 -0300

    Update choose-mode.js

--- src/rub/choose-mode.js
+++ src/rub/choose-mode.js
@@ -1,1 +1,12 @@
 
+var room = HBInit();
+
+room.pluginSpec = {
+  name: `rub/choose-mode`,
+  author: `ruben`,
+  version: `1.0.0`,
+  config: {},
+  dependencies: [],
+  order: {},
+  incompatible_with: [],
+};
